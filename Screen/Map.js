f3eb416e31f5a78cca582a42a180d95f42d67212
ChanannatM <chanannat10969@gmail.com>
2021-11-06 20:27:42 +0700

    map_marker

--- Screen/Map.js
+++ Screen/Map.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView ,{Marker} from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
+import { map } from 'traverse';
+import mapMarker from '../style/mapMarker';
 
 function Map({ navigation, route }) {
   const [state, setState] = useState({
@@ -12,27 +14,50 @@ function Map({ navigation, route }) {
       longitudeDelta: 0.005, //รัศมีจากตำแหน่ง lontitude
     },
     droplocationCords:{
-      latitude: 13.274305,
-      longitude: 100.928648,
+      latitude: 13.59984,
+      longitude: 100.721244,
       latitudeDelta: 0.03,
       longitudeDelta: 0.005,
     }
   })
 
+  const mapRef = useRef()
   const {pickupCords,droplocationCords} = state
   const GOOGLE_MAPS_APIKEY = 'AIzaSyB1-M05tikKf77sE5ccykQCdto5cuSPP0I';
 
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         style={StyleSheet.absoluteFill}
         initialRegion={pickupCords}
         > 
+        <Marker
+          coordinate = {pickupCords}
+          image={mapMarker.isCurLoc}
+        />
+         <Marker
+          coordinate = {droplocationCords}
+          image={mapMarker.isDesLoc}
+        />
+
         <MapViewDirections
           origin={pickupCords}
           destination={droplocationCords}
           apikey={GOOGLE_MAPS_APIKEY}
           strokeWidth={3}
+          strokeColor="hotpink"
+          optimizeWaypoints={true}
+          onReady={result => {
+            mapRef.current.fitToCoordinates(result.coordinates, {
+              edgePadding:{
+                right:30,
+                bottom:300,
+                left:30,
+                top:100
+              }
+            })
+          }}
         />
       </MapView>
      
---
+++ style/mapMarker.js
@@ -0,0 +1,4 @@
+export default{
+    isCurLoc: require('../images/rec.png'),
+    isDesLoc: require('../images/des.png')
+}
