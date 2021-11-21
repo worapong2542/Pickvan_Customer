8a3d4f54c6869975b4d7389eb27d5e63cf437601
worapong2542 <1810511101030@live4.utcc.ac.th>
2021-11-21 23:34:09 +0700

    alert_3

--- Screen/Routedetail.js
+++ Screen/Routedetail.js
@@ -1,55 +1,71 @@
-import { View, Text,StyleSheet,TouchableOpacity } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
 import axios from 'axios';
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Card from './Card';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ForceTouchGestureHandler} from 'react-native-gesture-handler';
 
-function Routedetail({ navigation, route }) {
-  const { item } = route.params;
-
-  const [userId, setuserId] = useState('')
+function Routedetail({navigation, route}) {
+  const {item} = route.params;
+  console.log()
+  const [userId, setuserId] = useState('');
 
   useEffect(() => {
     getUserId();
-  }, []); 
+  }, []);
 
-  async function getUserId() { 
+  async function getUserId() {
     const tempId = await AsyncStorage.getItem('@dataloginId');
-    setuserId(tempId)
+    setuserId(tempId);
   }
 
   async function sentbuyticket() {
-    await axios.post('http://10.0.2.2:3001/buy/buyticket', {
+    Alert.alert('ยืนยันรายการ', 'ท่านต้องการซื้อ ' + item[0].vandata.name +' วันที่ '+item[0].vandata.date.substring(0, 8)+''+(parseInt(item[0].vandata.date.substring(8, 10))+1)+ ' เวลา '+ item[0].vandata.time.substring(0, 5) + ' จำนวน ' + item[0].seat_select + ' ที่นั่ง เป็นเงิน '+item[0].vandata.price * item[0].seat_select + ' บาท', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => buyticket()},
+    ]);
+  }
+
+  async function buyticket() {
+    await axios
+      .post('http://10.0.2.2:3001/buy/buyticket', {
         user_id: userId,
         point_up: item[0].point_up_select,
         point_down: item[0].point_down_select,
         seat_amount: item[0].seat_select,
         schedule_id: item[0].vandata.id,
-        seat_all:item[0].vandata.seat_all,
+        seat_all: item[0].vandata.seat_all,
       })
       .then(res => call_back(res));
   }
+
   function call_back(res) {
-    if (res.data == "1") {
+    if (res.data == '1') {
       alert('Some thing Worng');
     } else {
       alert('จองเรียบร้อย');
-      item.push({ticketid:res.data}) //push ticketid เข้าใน item เพื่อเอาเลขตั๋วไปใช้หน้าต่อไป
-      navigation.navigate('ConfirmTicket', { item: { item } }) //ส่ง item รายละเอียดหน้ารี้ไป
+      item.push({ticketid: res.data}); //push ticketid เข้าใน item เพื่อเอาเลขตั๋วไปใช้หน้าต่อไป
+      navigation.navigate('ConfirmTicket', {item: {item}}); //ส่ง item รายละเอียดหน้ารี้ไป
     }
   }
 
   return (
-    <View style={{ flex: 1, backgroundColor: '#fff' }}>
-      <View style={{ flex: 2}}>
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <View style={{flex: 2}}>
         <Card>
-          <Text style={styles.textTime}>{item[0].vandata.time.substring(0, 5)}</Text> 
+          <Text style={styles.textTime}>
+            {item[0].vandata.time.substring(0, 5)}
+          </Text>
           <View style={{flexDirection: 'row'}}>
-            <View style={{ flex: 2}}>
+            <View style={{flex: 2}}>
               <Text style={styles.textDefault}>{item[0].vandata.name}</Text>
             </View>
 
-            <View style={{ flex: 1}}>
+            <View style={{flex: 1}}>
               <Text style={styles.textDefault}>{item[0].vandata.license}</Text>
             </View>
           </View>
@@ -57,56 +73,62 @@ function Routedetail({ navigation, route }) {
 
         <Card>
           <View style={{flexDirection: 'row'}}>
-            <View style={{ flex: 2}}>
+            <View style={{flex: 2}}>
               <Text style={styles.textDefault}>จุดขึ้น : </Text>
             </View>
 
-            <View style={{ flex: 1}}>
+            <View style={{flex: 1}}>
               <Text style={styles.textDefault}>{item[0].point_up_select}</Text>
             </View>
           </View>
 
           <View style={{flexDirection: 'row'}}>
-            <View style={{ flex: 2}}>
+            <View style={{flex: 2}}>
               <Text style={styles.textDefault}>จุดลง : </Text>
             </View>
 
-            <View style={{ flex: 1}}>
-              <Text style={styles.textDefault}>{item[0].point_down_select}</Text>
+            <View style={{flex: 1}}>
+              <Text style={styles.textDefault}>
+                {item[0].point_down_select}
+              </Text>
             </View>
           </View>
         </Card>
 
         <Card>
           <View style={{flexDirection: 'row'}}>
-            <View style={{ flex: 2}}>
+            <View style={{flex: 2}}>
               <Text style={styles.textDefault}>จำนวน : </Text>
             </View>
 
-            <View style={{ flex: 1}}>
-              <Text style={styles.textDefault}>{item[0].seat_select}       ที่</Text>
+            <View style={{flex: 1}}>
+              <Text style={styles.textDefault}>{item[0].seat_select} ที่</Text>
             </View>
           </View>
-          
+
           <View style={{flexDirection: 'row'}}>
-            <View style={{ flex: 2}}>
+            <View style={{flex: 2}}>
               <Text style={styles.textDefault}>รวม : </Text>
             </View>
 
-            <View style={{ flex: 1}}>
-              <Text style={styles.textDefault}>{item[0].vandata.price * item[0].seat_select}  บาท</Text>
+            <View style={{flex: 1}}>
+              <Text style={styles.textDefault}>
+                {item[0].vandata.price * item[0].seat_select} บาท
+              </Text>
             </View>
           </View>
         </Card>
       </View>
 
-      <View style={{ flex: 1}}>
-      {/* <TouchableOpacity onPress={() => sentbuyticket()}> */}
-      <TouchableOpacity onPress={() => sentbuyticket()}>
-        <View style={styles.btnConfirm}>
-          <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20, }}>จอง</Text>
-        </View>
-      </TouchableOpacity>
+      <View style={{flex: 1}}>
+        {/* <TouchableOpacity onPress={() => sentbuyticket()}> */}
+        <TouchableOpacity onPress={() => sentbuyticket()}>
+          <View style={styles.btnConfirm}>
+            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20}}>
+              จอง
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -115,7 +137,7 @@ function Routedetail({ navigation, route }) {
 export default Routedetail;
 
 const styles = StyleSheet.create({
-   textBold: {
+  textBold: {
     color: '#5660B3',
     fontWeight: 'bold',
     fontSize: 16,
@@ -131,10 +153,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
     marginBottom: 10,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   btnConfirm: {
-    marginTop:120,
+    marginTop: 120,
     margin: 20,
     backgroundColor: 'rgba(254, 181, 166, 1)',
     borderRadius: 40,
@@ -142,6 +164,6 @@ const styles = StyleSheet.create({
     width: '70%',
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
-})
+});
